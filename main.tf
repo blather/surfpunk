3c80bb6acf0d8e0b2d2f489e706afabeb937b8c4
R.Munden <randall.munden@gmail.com>
2020-03-16 09:34:00 -0500

    initial sp import

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+   required_version = "~> 0.12"
+   required_providers {
+     local = "~> 1.2"
+     aws = "2.38"
+   }   
+ }
+  
+provider "aws" {
+  region = "us-west-2"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "jimmi"
+}
+
+resource "aws_vpc" "ssm-test" {
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "surf-punk"
+  }
+}
+
